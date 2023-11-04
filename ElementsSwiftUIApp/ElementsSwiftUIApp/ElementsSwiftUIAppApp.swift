8e6cf0467fab54721a622883744cdab5bf77ecab
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-04 08:50:45 -0300

    Add files via upload

---
+++ ElementsSwiftUIApp/ElementsSwiftUIApp/ElementsSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElementsSwiftUIAppApp.swift
+//  ElementsSwiftUIApp
+//
+//  Created by Lucas Neves dos santos pompeu on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ElementsSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
